653a8fe68fb3253e82783df3a373a728e076d491
Staba1139 <takaaki2232mano@gmail.com>
2019-12-10 14:41:13 +0900

    [add] main.cpp          read values of Sensor

--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@
 #include "PID/PIDcontroller.h"
 
 
+LSM6DS33 sensor(p9, p10, LSM6DS33_AG_I2C_ADDR(1));
+
+Timer tmain;
+
+
+
 int main(){
+    sensor.begin();
+
+    double timeLog = 0;
+    tmain.start();
+    timeLog += tmain.read();
+
+    double mainPeriod = 0.01;
+
+    serial.printf("begin DroneControll\r\n");
+
+    while(1){
+
+    } 
 
 }
 
